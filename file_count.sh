77b06b5be64fc9116d994d4b01b160a6480062fc
salimp2009@gmail.com <salimp2009@gmail.com>
2023-04-08 19:58:27 +0300

    added file_count that counts the number of files in a directory ; refactor with find because ls will include directories as well

---
+++ file_count.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+read -r -p "Please enter a directory name to count num of files: " DIR
+file_count() {
+	if [[ -f "${DIR}" ]]; then
+		echo "you entered a file name; not a directory!"
+		return 1
+	else
+		local NUM_FILES
+		NUM_FILES=$(ls "${DIR}" | wc -l)
+		if [[ "$?" -eq 0 ]]; then
+			echo "${DIR}"
+			echo "${NUM_FILES}"
+			return 0
+		fi
+		echo "something went wrong!"
+		return 1
+	fi
+}
+
+file_count
+SUCCESS_FAIL="$?"
+if [[ ${SUCCESS_FAIL} != 0 ]]; then
+	echo "something went wrong, please try again!"
+	exit 1
+fi
